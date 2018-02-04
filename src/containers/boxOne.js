2c61adbe9ebcb03a458977784a759c9df200f46a
John Duncan <john@johnduncanphoto.com>
2018-02-04 15:52:00 +0000

    click event and log added on fist box

--- src/containers/boxOne.js
+++ src/containers/boxOne.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 class BoxOne extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {toggle: true}
+        this.eventHandler = this.eventHandler.bind(this)
+    }
+
+    eventHandler(event) {
+        this.setState((prevState) => ({
+            toggle: !prevState.toggle
+        }),
+        console.log(this.state)
+        );
+    }
+
+
+
 
     render() {
         return (
-            <div className="box">
+            <div className="box"
+                onClick={this.eventHandler}>
                 <h1>Box One</h1>
             </div>
         )
