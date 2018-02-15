4eae3cf74df516868b68b978120117313bc810e9
John Duncan <john@johnduncanphoto.com>
2018-02-15 14:02:30 +0000

    back button created

---
+++ src/containers/backButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class BackButton extends React.Component {
+
+
+
+    render() {
+        return (
+            <div className="back-button">
+
+            </div>
+        )
+    }
+
+}
+
+export default BackButton;
