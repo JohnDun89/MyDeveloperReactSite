e2b1274a86eb6f8515f6380e128b923a289de25d
John Duncan <john@johnduncanphoto.com>
2018-02-28 15:29:49 +0000

    links added to github box

--- client/src/containers/github.js
+++ client/src/containers/github.js
@@ -4,26 +4,36 @@ import Link from './link.js';
 class Github extends React.Component {
     constructor(props) {
        super(props)
-        this.props ={
-            Github: ['GitHub','https://github.com/JohnDun89'],
-            Budgeting: ['Budgeting App','https://github.com/JohnDun89/budgeting_app'],
-            Cycling: ['Cycling Route Finder','https://github.com/JohnDun89/js-group-project'],
-            ToDo: ['Android To Do List', 'https://github.com/JohnDun89/AndroidToDoList'],
-            ReactProject: ['This Site', 'https://github.com/JohnDun89/MyDeveloperReactSite'],
-            Pagination: ['Pagination with Game of Thrones API','https://github.com/JohnDun89/Game-0f-Thrones-Christmas-Project']
+        this.state ={
+            Github: ['GitHub','https://github.com/JohnDun89','All projects and CodeClan Homeworks.'],
+            Budgeting: ['Budgeting App','https://github.com/JohnDun89/budgeting_app','A Budgeting App, built with Ruby, Sinatra, CSS and SQL'],
+            Cycling: ['Cycling Route Finder','https://github.com/JohnDun89/js-group-project','Group Project, using Javascript, NodeJS, MondgoDB and CSS'],
+            ToDo: ['Android To Do List', 'https://github.com/JohnDun89/AndroidToDoList','Android Phone App, Java, Android Studio and SQLite'],
+            ReactProject: ['This Site', 'https://github.com/JohnDun89/MyDeveloperReactSite','This site, React, SCSS and WebGl'],
+            Pagination: ['Pagination with Game of Thrones API','https://github.com/JohnDun89/Game-0f-Thrones-Christmas-Project','']
         }
+
+        
     }
 
+    ComponentWillMount () {
+
+    }
    
 
     render() {
 
-
+              
         return (
-            <div >
+            <div className="expanded-content">
                 <button className="back-button" onClick={this.props.close} type="button" >Close</button>
                 <div >
                     <h1>Links to git hub and specific projects</h1>
+                    <Link text={this.state.Github} />
+                    <Link text={this.state.Budgeting}/>
+                    <Link text={this.state.Cycling}/>
+                    <Link text={this.state.ToDo}/>
+                    <Link text={this.state.ReactProject}/>
                 </div>
             </div>
         )
--- client/src/containers/link.js
+++ client/src/containers/link.js
@@ -3,16 +3,30 @@ import React from 'react'
 class Link extends React.Component {
         constructor(props){
             super(props)
+        this.linkClicked = this.linkClicked.bind(this)
         }
 
+
+    componentDidMount() {
+      
+    }
+
+    linkClicked() {
+        console.log("clicked")
+    }  
+
     render() {
+
+        const title = this.props.text[0]
+        const link = this.props.text[1]
+        const keywords = this.props.text[2]
         return (
 
-            <div >
-                <button className="back-button" onClick={this.props.close} type="button" >Close</button>
+            <div className="list-item" onClick={this.linkClicked}>
                 <div >
-                    <h1>Links to git hub and specific projects</h1>
-
+                    
+                    <h1 className="li-title">{title}</h1>
+                    <h2 className="li-keywords">{keywords}</h2>
                 </div>
             </div>
         )
