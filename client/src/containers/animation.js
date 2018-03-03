591028a57507cd2f0075634a1a661036ebe1fc4b
John Duncan <john@johnduncanphoto.com>
2018-03-03 14:35:13 +0000

    mouse hovers now detected and logged as coords in animation

--- client/src/containers/animation.js
+++ client/src/containers/animation.js
@@ -2,31 +2,44 @@
 import React from 'react';
 import BackButton from './backButton.js'
 import Tilt from 'react-tilt'
-import Hover from './hover-pan.js'
+import $ from 'jquery'; 
+
 class Animation extends React.Component {
 
     constructor(props) {
         super(props)
+
+        this.state = { x: 0, y: 0 };
+
+    }
+
+    _onMouseMove(e) {
+        const position = 
+        
+        console.log(position, e.nativeEvent.offsetX, e.screenX);
+
+        this.setState({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
     }
 
     render() {
+        const { x, y } = this.state;
         return (
-            <div className="expanded-content">
+            <div className="expanded-content" ref="elem">
                 <div className="button-container">
                     <button className="back-button" onClick={this.props.close} type="button" >Close</button>
                 </div>
-                <div id="animation-container">
-                    <img id="image-castle" className="image" src="./assets/background-01.png" />
+                <div id="animation-container" onMouseMove={this._onMouseMove.bind(this)}>
+                    <img id="image-castle" className="image" src="./assets/background-01.png"  />
                     <img id="image-back" className="image" src="./assets/MidGround-01.png"/>
                     <img id="image-mid" className="image" src="./assets/FrontMidground-01.png" />
                     <img id="image-front" className="image" src="./assets/ForeGround-01.png" />                 
                 </div>      
                 <p>This will contain links to my twitter and git hub</p>
+                <h1>Mouse coordinates: {x} {y}</h1>
             </div>
 
         )
     }
-
+    
 }
-
 export default Animation;
