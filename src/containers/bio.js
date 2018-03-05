e0e5be2165c4324d3c4b6594452adc28c011a358
John Duncan <john@johnduncanphoto.com>
2018-03-05 16:48:55 +0000

    phone number removed

--- src/containers/bio.js
+++ src/containers/bio.js
@@ -10,7 +10,7 @@ class Bio extends React.Component {
         return (
             <div id="bio">
                 <div id="contact details">
-                <h1>0789 66 00 3989</h1>
+               
                 <h1>john@johnduncanphoto.com</h1>
                 </div>
                 <div>
